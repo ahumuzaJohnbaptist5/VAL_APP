548dca417c41ba84e649695231499cd00bb2ec1a
Ahumuza John Baptist <262724420+ahumuzaJohnbaptist5@users.noreply.github.com>
2026-07-17 22:13:53 +0300

    Delete old directory

--- old/valcakes-backend/catalog/admin.py
+++
@@ -1,6 +0,0 @@
-# catalog/admin.py
-from django.contrib import admin
-from .models import Category, Cake
-
-admin.site.register(Category)
-admin.site.register(Cake)
--- old/valcakes-backend/orders/urls.py
+++
@@ -1,9 +0,0 @@
-# orders/urls.py
-from django.urls import path
-from .views import CreateOrderView, AgentOrderListView, VerifyPaymentView
-
-urlpatterns = [
-    path('create/', CreateOrderView.as_view(), name='create-order'),
-    path('agent/list/', AgentOrderListView.as_view(), name='agent-order-list'),
-    path('payments/<int:payment_id>/verify/', VerifyPaymentView.as_view(), name='verify-payment'),
-]
